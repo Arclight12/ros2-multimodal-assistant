8eb4f72b12570407a37e045f0b83e9440bfc7a40
nekhss <nekharighaa@gmail.com>
2026-09-09 09:47:16 +0530

    Add 4-DOF robot description and RViz visualization

---
+++ src/assistant_description/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'assistant_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*')
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*')
+        ),
+        (
+            os.path.join('share', package_name, 'rviz'),
+            glob('rviz/*')
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='awtistic',
+    maintainer_email='awtistic@todo.todo',
+    description='URDF and RViz description for the 4-DOF robotic assistant arm',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
